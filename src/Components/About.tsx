42e444c79dfd72d428be75978898740eedcb4eff
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-04-26 13:23:45 +0100

    Adding icons

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,4 +1,7 @@
 import "../CSS/About.scss"
+import codeImage from "../assets/codeImage.png"
+import branchImage from "../assets/branchImage.png"
+import gearImage from "../assets/gearImage.jpg"
 
 function About() {
   return (
@@ -6,8 +9,9 @@ function About() {
       <div className="main-content">
         <h1 className="title">Welcome to my portfolio</h1>
         <h2 className="subtitle">
-          I am a London-based Software Engineer looking for frontend and
-          fullstack developer roles.
+          I am a London-based Software Engineer with two years of commercial
+          experience building web applications using Vue.js, TypeScript,
+          JavaScript and Python.
         </h2>
         <button className="lets-talk-button">
           <div className="button-background"></div>
@@ -15,13 +19,9 @@ function About() {
         </button>
       </div>
       <div className="extra-content">
-        <div>Commercial experience with:</div>
-        <ul>
-          <li>Vue.js</li>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-          <li>Python</li>
-        </ul>
+        <img className="images code-image" src={codeImage} alt="coding" />
+        <img className="images branch-image" src={branchImage} alt="branches" />
+        <img className="images gear-image" src={gearImage} alt="gears" />
       </div>
     </div>
   )
