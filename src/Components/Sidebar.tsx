28299115b723c65192c9420bac2236032f20ddcc
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-04-26 14:01:25 +0100

    Adding icons for github and linkedin

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,18 +1,29 @@
 import "../CSS/Sidebar.scss"
+import cvImage from "../assets/cvImage.jpg"
+import githubImage from "../assets/githubImage.png"
+import linkedinImage from "../assets/linkedinImage.jpg"
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="links-container">
-        <div className="cv">
-          <span>CV</span>
-        </div>
-        <div className="github">
-          <span>GH</span>
-        </div>
-        <div className="linkedin">
-          <span>LI</span>
-        </div>
+        <a href="/">
+          <img src={cvImage} alt="cv" />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/olliecase-green"
+        >
+          <img src={githubImage} alt="github" />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/oliver-case-green-40001416a/"
+        >
+          <img src={linkedinImage} alt="linkedin" />
+        </a>
       </div>
     </div>
   )
