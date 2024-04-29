5cc810167181971fbea8b8a9a269c75de23c01fd
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-04-29 12:19:57 +0100

    Remove redundant grid spacing and change about page href

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useRef } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./Components/Navbar"
-import Sidebar from "./Components/Sidebar"
 import About from "./Components/About"
 import Experience from "./Components/Experience"
 import Projects from "./Components/Projects"
@@ -13,9 +11,8 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        {/* <Sidebar /> */}
         <Routes>
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<About />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
