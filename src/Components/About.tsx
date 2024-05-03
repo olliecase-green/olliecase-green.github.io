8bebcaffaf4b73cee59b16998c00390e3019259f
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-05-03 13:51:28 +0100

    Update about page and footer with upcoming features

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import "../CSS/About.scss"
 
 function About() {
+  const [letsTalk, setLetsTalk] = useState(false)
+
+  function handleClick() {
+    setLetsTalk(true)
+  }
+
   return (
     <div className="about-container">
       <div className="main-content">
@@ -9,27 +16,54 @@ function About() {
           Software Engineer with two years of commercial experience.
         </h2>
         <div className="about-text">
-          Welcome to my portfolio! "Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum."
+          Welcome to my portfolio! I am a London-based Software Engineer with
+          commercial experience using Vue.js, JavaScript, TypeScript and Python.
+          I am ideally looking for Frontend and Full-stack Developer positions
+          in my next position. I am also very confident working with React from
+          my full-time coding bootcamp, Sigma Labs XYZ, and Django from my
+          personal projects - please check out my projects in the navbar above!
         </div>
         <div className="about-text">
-          Welcome to my portfolio! "Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum."
+          In my spare time, I have been teaching myself Django for backend
+          development, initially from online documentation and more recently
+          from a book: <span className="django-text">Django 4 By Example</span>.
         </div>
-        <button className="lets-talk-button">
+        <h3 className="additional-info">Site info and additional features</h3>
+        <div className="about-text">
+          This website was built using React and TypeScript and is hosted on
+          GitHub pages. To navigate between pages, I used React Router.
+          Additional features which are not yet finished include:
+          <ul>
+            <li>
+              The "Let's talk!" button which will open an email from the user to
+              me.
+            </li>
+            <li>
+              The Joke button at the bottom of the page will fetch a joke from a
+              free joke api: https://icanhazdadjoke.com/api.
+            </li>
+            <li>
+              There will be a light / dark mode option to change the theme of
+              the site. I am planning to use React Context API to globally store
+              whether dark mode is activated, and update the styling in each
+              component accordingly.
+            </li>
+            <li>The site needs mobile and smaller screen size styling</li>
+          </ul>
+        </div>
+        <div className="about-text">
+          The source code can be seen in my GitHub repository:
+          https://github.com/olliecase-green/olliecase-green.github.io
+        </div>
+        <button className="lets-talk-button" onClick={handleClick}>
           <div className="button-background"></div>
           <span>Let's talk!</span>
         </button>
+        {letsTalk && (
+          <div className="lets-talk-div">
+            This button will open up an email to my email address from the user
+          </div>
+        )}
       </div>
     </div>
   )
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,14 +6,24 @@ function Footer() {
   const [joke, setJoke] = useState<Joke>(null)
 
   function handleClick() {
-    setJoke("Why did the chicken cross the road?")
+    setJoke("Why did the chicken cross the road? To get to the other side")
   }
 
   return (
     <div className="footer-container">
-      <button onClick={handleClick}>Tell me a joke</button>
-      <div>{joke}</div>
-      {joke && <div>Did you know this joke was fetched from free joke api</div>}
+      {joke ? (
+        <div className="joke-info">
+          <div className="joke">{joke}</div>
+          <div>
+            This joke is hard-coded at the moment but will soon be fetched from
+            a free api
+          </div>
+        </div>
+      ) : (
+        <button className="joke-button" onClick={handleClick}>
+          Tell me a joke
+        </button>
+      )}
     </div>
   )
 }
